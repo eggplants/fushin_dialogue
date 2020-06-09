6bac8557ce438e2f223c6d567f55a4d9d790f6d8
eggplants <w10776e8w@yahoo.co.jp>
2020-06-10 05:31:28 +0900

    Add table maker

--- CSVToMD.py
+++ CSVToMD.py
@@ -1,12 +1,23 @@
 import pandas as pd
 from pytablewriter import MarkdownTableWriter
 
+
 def main():
     path = 'dialogue_all.csv'
     df = pd.read_csv(path, sep=',')
     w = MarkdownTableWriter()
     w.from_dataframe(df)
-    print(w.write_table())
+    with open("dialogue_all.md", "w") as u:
+        w.stream = u
+        w.write_table()
+    d = ""
+    with open("dialogue_all.md", "r") as u:
+        d = u.read()
+        d = d.replace(" ", "")
+    with open("dialogue_all.md", "w") as u:
+        u.write("# 一覧表\n\n")
+        u.write(d)
+
 
 if __name__ == "__main__":
     main()
